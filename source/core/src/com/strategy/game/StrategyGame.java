fe7fead45e71e19c2943f1b044cfff1737a5c652
Alex <taier93@gmail.com>
2016-04-21 22:06:55 +0200

    Added a couple of config options to the application instance. Switched ApplicationAdapter with Game as superclass of the game class.

--- source/core/src/com/strategy/game/StrategyGame.java
+++ source/core/src/com/strategy/game/StrategyGame.java
@@ -1,12 +1,18 @@
 package com.strategy.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class StrategyGame extends ApplicationAdapter {
+/*
+	The main game object, extends the Game class that easily allows
+	multiple screens (e.g. main menu, game screen and so on).
+ */
+
+public class StrategyGame extends Game {
 	SpriteBatch batch;
 	Texture img;
 	
--- source/desktop/src/com/strategy/game/desktop/DesktopLauncher.java
+++ source/desktop/src/com/strategy/game/desktop/DesktopLauncher.java
@@ -7,6 +7,10 @@ import com.strategy.game.StrategyGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 1280;
+		config.height = 720;
+		config.title = "A Strategy Game [Working title]";
+		config.vSyncEnabled = true;
 		new LwjglApplication(new StrategyGame(), config);
 	}
 }
