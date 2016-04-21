e1f6eb6ba065d3de02c6f718467a8136e50e0e39
Alex <taier93@gmail.com>
2016-04-21 19:42:26 +0200

    Generated test project, probably doesn't work.

---
+++ source/desktop/src/com/strategy/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.strategy.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.strategy.game.StrategyGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new StrategyGame(), config);
+	}
+}
