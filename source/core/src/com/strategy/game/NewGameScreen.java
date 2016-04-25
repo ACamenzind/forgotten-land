4a600fc1ad7a503cc5f1e0217256a107e2361a30
Alex <taier93@gmail.com>
2016-04-25 19:42:22 +0200

    Added placeholder classes for the various screens/menus.

---
+++ source/core/src/com/strategy/game/NewGameScreen.java
@@ -0,0 +1,49 @@
+package com.strategy.game;
+
+import com.badlogic.gdx.Screen;
+
+/**
+ * Screen that appears when selecting "new game" in the main menu
+ * Shows options before starting a game (i.e. map generator).
+ * TODO: implement
+ */
+public class NewGameScreen implements Screen{
+
+    public NewGameScreen(StrategyGame game) {
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
---
+++ source/core/src/com/strategy/game/SettingsScreen.java
@@ -0,0 +1,48 @@
+package com.strategy.game;
+
+import com.badlogic.gdx.Screen;
+
+/**
+ * A screen that contains various settings for the game. Can be reached from the main menu.
+ * TODO: implement
+ */
+public class SettingsScreen implements Screen {
+
+    public SettingsScreen() {
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
