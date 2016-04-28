03973a14902330a0de03280d849d1dc6d2f391d4
Alexander Fischer <taier93@gmail.com>
2016-04-28 09:49:58 +0200

    Added placeholder pause screen.

---
+++ source/core/src/com/strategy/game/PauseScreen.java
@@ -0,0 +1,53 @@
+package com.strategy.game;
+
+import com.badlogic.gdx.Screen;
+
+/**
+ * The screen that appears when pausing the game.
+ * Should contain the following buttons:
+ * - Resume: returns to the game screen and unpauses the game
+ * - Save game: saves the current state of the game.
+ * - Load game
+ * - Quit to main menu
+ * - Quit to desktop
+ */
+public class PauseScreen implements Screen{
+
+    public PauseScreen() {
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
