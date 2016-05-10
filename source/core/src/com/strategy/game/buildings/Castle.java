617522837c64e650953ee8e2d282f3c74175b47f
Alexander Fischer <taier93@gmail.com>
2016-05-10 08:39:48 +0200

    Buildings (castle, wall and house) now extend from Building, and they now have properties such as cost, resources.

--- source/core/src/com/strategy/game/buildings/Castle.java
+++ source/core/src/com/strategy/game/buildings/Castle.java
@@ -2,13 +2,15 @@ package com.strategy.game.buildings;
 
 import com.badlogic.gdx.math.Vector2;
 import com.strategy.game.Assets;
+import com.strategy.game.Building;
+import com.strategy.game.ResourceContainer;
 
 /**
- * A type of MapEntity
+ * A type of Building
  */
-public class Castle extends MapEntity {
+public class Castle extends Building {
     public Castle() {
-        super();
+        super("castle", new ResourceContainer(100,100,100,100,5), new ResourceContainer(0,0,0,0,0), 100, 10);
         this.mainTexture = Assets.castle;
 //        this.imgOffset = new Vector2(1,50);
         this.collisionSize = new Vector2(4, 5);
