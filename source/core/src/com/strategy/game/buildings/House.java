77bc6b3da5127058d6ebee7582c0beb88bacb114
Amedeo <amedeo.zucchetti@usi.ch>
2016-05-11 13:46:04 +0200

    updated house1.png

--- source/core/src/com/strategy/game/buildings/House.java
+++ source/core/src/com/strategy/game/buildings/House.java
@@ -12,7 +12,7 @@ public class House extends Building {
     public House() {
         super("castle", new ResourceContainer(100,100,100,100,5), new ResourceContainer(0,0,0,0,0), 100, 10);
         this.mainTexture = Assets.house1;
-        this.collisionSize = new Vector2(3,3);
+        this.collisionSize = new Vector2(2,2);
 //        this.imgOffset = new Vector2(0,1);
         sliceTexture(mainTexture);
     }
