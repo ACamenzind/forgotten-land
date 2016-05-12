25cfc14db13f04d257f50ced1f5c17811b543da8
Francesco Sani <francescosani@francescos-mbp.mobile.usilu.net>
2016-05-12 16:16:30 +0200

    renaming for coherence

--- source/core/src/com/strategy/game/buildings/CollectorRock.java
+++ source/core/src/com/strategy/game/buildings/CollectorRock.java
@@ -5,7 +5,7 @@ import com.strategy.game.ResourceContainer;
 /**
  * Created by francescosani on 12/05/16.
  */
-public class CollectorStone extends Building {
+public class CollectorRock extends Building {
     private static final String NAME = "Mine";
     private static final ResourceContainer COST = new ResourceContainer(40, 0, 10, 0, 0);
     private static final ResourceContainer PRODUCTION = new ResourceContainer(10, 0, 0, 1, 0);
@@ -16,7 +16,7 @@ public class CollectorStone extends Building {
     private int life = MAX_LIFE;
     private int workers = 0;
 
-    public CollectorStone() {
+    public CollectorRock() {
         super(NAME, COST, PRODUCTION, MAINTENANCE, MAX_LIFE, MAX_WORKERS,INFLUENCE_RADIUS);
     }
 }
--- source/core/src/com/strategy/game/buildings/WarehouseRock.java
+++ source/core/src/com/strategy/game/buildings/WarehouseRock.java
@@ -5,7 +5,7 @@ import com.strategy.game.ResourceContainer;
 /**
  * Created by francescosani on 12/05/16.
  */
-public class WarehouseStone extends Building {
+public class WarehouseRock extends Building {
     private static final String NAME = "Stone Warehouse";
     private static final ResourceContainer COST = new ResourceContainer(40, 0, 10, 0, 0);
     private static final ResourceContainer PRODUCTION = new ResourceContainer(10, 0, 0, 1, 0);
@@ -17,7 +17,7 @@ public class WarehouseStone extends Building {
     private int life = MAX_LIFE;
     private int workers = 0;
 
-    public WarehouseStone() {
+    public WarehouseRock() {
         super(NAME, COST, PRODUCTION, MAINTENANCE, MAX_LIFE, MAX_WORKERS,INFLUENCE_RADIUS);
     }
 }
