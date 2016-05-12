153ddf0d6f0ff7a1ba11c05bacbe5f93d63a4962
Francesco Sani <francescosani@francescos-mbp.mobile.usilu.net>
2016-05-12 16:21:32 +0200

    modified collectors

--- source/core/src/com/strategy/game/buildings/CollectorWood.java
+++ source/core/src/com/strategy/game/buildings/CollectorWood.java
@@ -1,5 +1,7 @@
 package com.strategy.game.buildings;
 
+import com.badlogic.gdx.math.Vector2;
+import com.strategy.game.Assets;
 import com.strategy.game.ResourceContainer;
 
 /**
@@ -7,9 +9,9 @@ import com.strategy.game.ResourceContainer;
  */
 public class CollectorWood extends Building {
     private static final String NAME = "Lumber camp";
-    private static final ResourceContainer COST = new ResourceContainer(40, 0, 10, 0, 0);
-    private static final ResourceContainer PRODUCTION = new ResourceContainer(10, 0, 0, 1, 0);
-    private static final ResourceContainer MAINTENANCE = new ResourceContainer(10, 0, 0, 1, 0);
+    private static final ResourceContainer COST = new ResourceContainer(40, 0, 10, 5, 0);
+    private static final ResourceContainer PRODUCTION = new ResourceContainer(10, 0, 0, 0, 0);
+    private static final ResourceContainer MAINTENANCE = new ResourceContainer(0, 0, 1, 1, 0);
     private static final int MAX_LIFE = 10;
     private static final int MAX_WORKERS = 5;
     private static final int INFLUENCE_RADIUS = 5;
@@ -18,5 +20,50 @@ public class CollectorWood extends Building {
 
     public CollectorWood() {
         super(NAME, COST, PRODUCTION, MAINTENANCE, MAX_LIFE, MAX_WORKERS,INFLUENCE_RADIUS);
+    }
+
+
+    public static String getNAME() {
+        return NAME;
+    }
+
+    public static ResourceContainer getCOST() {
+        return COST;
+    }
+
+    public static ResourceContainer getPRODUCTION() {
+        return PRODUCTION;
+    }
+
+    public static ResourceContainer getMAINTENANCE() {
+        return MAINTENANCE;
+    }
+
+    public static int getMaxLife() {
+        return MAX_LIFE;
+    }
+
+    public static int getMaxWorkers() {
+        return MAX_WORKERS;
+    }
+
+    public static int getInfluenceRadius() {
+        return INFLUENCE_RADIUS;
+    }
+
+    public int getLife() {
+        return life;
+    }
+
+    public void setLife(int life) {
+        this.life = life;
+    }
+
+    public int getWorkers() {
+        return workers;
+    }
+
+    public void setWorkers(int workers) {
+        this.workers = workers;
     }
 }
