cd8cb3e8b66d4dc7a3798d810e90d5073277a565
Amedeo <amedeo.zucchetti@usi.ch>
2016-05-19 20:50:15 +0200

    Blocked window resizing. Again.

--- source/desktop/src/com/strategy/game/desktop/DesktopLauncher.java
+++ source/desktop/src/com/strategy/game/desktop/DesktopLauncher.java
@@ -15,7 +15,7 @@ public class DesktopLauncher {
         config.useHDPI = Utils.HDPI;
 		config.title = "A Strategy Game [Working title]";
 		config.vSyncEnabled = true;
-		config.resizable = true;
+		config.resizable = false;
 		new LwjglApplication(new StrategyGame(), config);
 	}
 }
