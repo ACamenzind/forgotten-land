02080c99eb639d63fae12070450c6c60cd4bced0
Alex <taier93@gmail.com>
2016-05-21 12:04:36 +0200

    Made container class abstract.

--- source/core/src/com/strategy/game/buildings/Container.java
+++ source/core/src/com/strategy/game/buildings/Container.java
@@ -7,7 +7,7 @@ import com.strategy.game.ResourceContainer;
 /**
  * Created on 21/05/2016.
  */
-public class Container extends Building {
+public abstract class Container extends Building {
     protected ResourceContainer resourcesStored;
 
     public Container(String name,
