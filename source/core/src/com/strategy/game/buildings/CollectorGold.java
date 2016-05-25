36d8c5984d7a6cec1a9533d0ca3e830314aba350
Alex <taier93@gmail.com>
2016-05-25 09:26:24 +0200

    Added market building class (no asset yet).

---
+++ source/core/src/com/strategy/game/buildings/CollectorGold.java
@@ -0,0 +1,25 @@
+package com.strategy.game.buildings;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
+import com.strategy.game.Assets;
+import com.strategy.game.ResourceContainer;
+
+/**
+ * The market, which produces gold.
+ */
+public class CollectorGold extends Building{
+    private static final String NAME = "Market";
+    private static final ResourceContainer COST = new ResourceContainer(40, 0, 10, 5, 0);
+    private static final ResourceContainer PRODUCTION = new ResourceContainer(0, 0, 0, 3, 0);
+    private static final ResourceContainer MAINTENANCE = new ResourceContainer(1, 0, 1, 0, 0);
+    private static final int MAX_LIFE = 100;
+    private static final int MAX_WORKERS = 5;
+    private static final int INFLUENCE_RADIUS = 5;
+    private static final Texture TEXTURE = Assets.cows;
+    private static final Vector2 COLLISION = new Vector2(3,3);
+
+    public CollectorGold() {
+        super(NAME, COST, PRODUCTION, MAINTENANCE, MAX_LIFE, MAX_WORKERS,INFLUENCE_RADIUS, TEXTURE, COLLISION, Building.BuildingType.MANUFACTURER);
+    }
+}
