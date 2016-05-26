6e8f1a5232ba9970e88e0ba8d94b46e412bf3fcb
samuelebischof <samuele.bischof@usi.ch>
2016-05-26 18:30:27 +0200

    Changed positioning of the NewGame JFrame

--- source/desktop/src/com/strategy/game/desktop/DesktopLauncher.java
+++ source/desktop/src/com/strategy/game/desktop/DesktopLauncher.java
@@ -2,12 +2,16 @@ package com.strategy.game.desktop;
 
 import com.strategy.game.NewGame;
 
+import java.awt.*;
+
 // TODO: add texture packing which generates texture atlases
 
 public class DesktopLauncher {
 	public static boolean HDPI;
 	public static void main (String[] arg) {
 		NewGame frame = new NewGame("Resolution");
+		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+		frame.setLocation(dim.width/2-frame.getSize().width/2, dim.height/2-frame.getSize().height/2);
 		frame.setSize( 250, 100 );
 		frame.setVisible( true );
 	}
